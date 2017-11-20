ad0771e5306121d558ab2f4804e9eb0f3df716b2
Athiwatp <athiwatp@gmai.com>
2017-11-20 16:02:38 +0700

    create generateMessage

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const path = require('path');
 const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
+const {generateMessage} = require('./utils/message');
 
 const publicPath = path.join(__dirname, "../public");
 const port = process.env.PORT || 3000;
@@ -16,25 +17,18 @@ io.on('connection', (socketio) => {
     console.log('New use Connected');
 
 
-    socketio.emit('newMessage' , {
-       from:"Admin" ,
-       text:"Welcome to the chat app" ,
-        createAt : new Date().getTime()
-    });
+    socketio.emit('newMessage', generateMessage('Admin', 'Welcom to the chat app'));
+    socketio.broadcast.emit('newMessage', generateMessage('Admin', 'New User to joined'));
 
-    socketio.broadcast.emit('newMessage' , {
-        from:"Admin" ,
-        text:"New User to joined" ,
-        createAt : new Date().getTime()
-    });
+    // socketio.broadcast.emit('newMessage' , {
+    //     from:"Admin" ,
+    //     text:"New User to joined" ,
+    //     createAt : new Date().getTime()
+    // });
 
     socketio.on('createMessage', (message) => {
         console.log('createMessage : ', message);
-        // io.emit('newMessage', {
-        //     from: "Bot",
-        //     text: "สบายดี คุณล่ะ สบายดีไหม",
-        //     createAt: new Date().getTime()
-        // });
+        io.emit('newMessage', generateMessage(message.from, message.text));
         // socketio.broadcast.emit('newMessage' , {
         //     from:message.from,
         //     text:message.text,
